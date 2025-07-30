3173d504ccf64fb5c216328bd14f3770bd664130
livingtoonehundred <mattmilk76@gmail.com>
2025-07-30 23:17:20 +0100

    Create toaster.tsx

---
+++ client/src/components/ui/toaster.tsx
@@ -0,0 +1,9 @@
+import { Toast } from "./toast"
+
+export function Toaster() {
+  return (
+    <div className="fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]">
+      {/* Toast container */}
+    </div>
+  )
+}

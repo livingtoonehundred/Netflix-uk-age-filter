8b53caa4ca603eb97b92bcad11e8b3cc8cc401c8
livingtoonehundred <mattmilk76@gmail.com>
2025-07-31 21:47:50 +0100

    Create theme-provider.tsx

---
+++ client/src/components/theme-provider.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+export interface ThemeProviderProps {
+  children: React.ReactNode
+  defaultTheme?: "light" | "dark" | "system"
+  storageKey?: string
+}
+
+export const ThemeProvider: React.FC<ThemeProviderProps> = ({ 
+  children, 
+  defaultTheme = "dark",
+  storageKey = "theme" 
+}) => {
+  return <>{children}</>
